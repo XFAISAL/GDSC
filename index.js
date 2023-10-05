4d57ee7eded57261e8c0399a1392f07521226d3b
Faisal <baeesa@gmail.com>
2023-10-05 21:48:47 +0300

    question list + Swal Update

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
+const questions = [
+    {
+        question: 'هل تشعرين بألم غير عادي في الثدي؟',
+        key: 'q1'
+    },
+    {
+        question: 'هل تظهر أو تسمع صدور غير عادية؟',
+        key: 'q2'
+    },
+];
+
+let answers = {};
+
+function displayQuestion(index) {
+    if (index < questions.length) {
+        Swal.fire({
+            title: questions[index].question,
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'نعم',
+            cancelButtonText: 'لا'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                answers[questions[index].key] = 'نعم';
+            } else {
+                answers[questions[index].key] = 'لا';
+            }
+            displayQuestion(index + 1);
+        });
+    } else {
+        displayResults();
+    }
+}
+
+function displayResults() {
+    let score = 0;
+
+    let resultText = 'نتائج الاختبار:\n';
+    for (const question of questions) {
+        if (answers[question.key] == 'نعم') score++;
+
+        resultText += `${question.question}: ${answers[question.key]}\n`;
+    }
+
+    let percentage = (score / questions.length) * 100;
+    if (percentage >= 50) {
+        resultText += `نسبة الاصابة بسرطان الثدي: ${percentage}%\n`;
+        resultText += 'ننصحك بالذهاب للمستشفى للفحص';
+    }else{
+        resultText += `نسبة الاصابة بسرطان الثدي: ${percentage}%\n`;
+        resultText += 'لا داعي للقلق';
+    }
+
+    Swal.fire({
+        title: 'النتائج',
+        text: resultText,
+        icon: 'info'
+    });
+}
 
 window.onload = function () {
 
@@ -7,42 +68,7 @@ window.onload = function () {
         let _piece = pieces[i];
         _piece.onclick = function(t) {
             if(t.target.parentElement.getAttribute('data-position') == "chest"){
-                Swal.fire({
-                    title: 'مرحبا',
-                    text: "هل تشعرين بأعراض تشير إلى إمكانية وجود سرطان الثدي؟",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: '!نعم',
-                    cancelButtonText: 'لا'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        Swal.fire({
-                            title: 'السؤال الأول',
-                            text: "هل لديك ألم في الثدي؟",
-                            icon: 'question',
-                            showCancelButton: true,
-                            confirmButtonColor: '#3085d6',
-                            cancelButtonColor: '#d33',
-                            confirmButtonText: '!نعم',
-                            cancelButtonText: 'لا'
-                        }).then((result) => {
-                            if (navigator.geolocation) {
-                                navigator.geolocation.getCurrentPosition(showPosition);
-                            } else {
-                                alert('المتصفح لا يدعم ميزة تحديد الموقع.');
-                            }
-                            if(result.isConfirmed){
-                                Swal.fire(
-                                    'ننصح بالتوجه للطبيب لتقييم الحالة!',
-                                    'الوصول الى اقرب عيادة',
-                                    'success'
-                                )
-                            }
-                        })
-                    }
-                })
+                displayQuestion(0)
             }
             if (t.target.getAttribute('data-position') != null) document.getElementById('data').innerHTML = t.target.getAttribute('data-position');
             if (t.target.parentElement.getAttribute('data-position') != null) document.getElementById('data').innerHTML = t.target.parentElement.getAttribute('data-position');
